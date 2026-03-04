3a9361f06f6d885564c84afc32dc3ebbd36acedf
Victor <v.denis@avanade.com>
2026-03-04 15:32:59 +0100

    refactor: passage aux variables et ajout d'un subnet

--- resources.tf
+++ resources.tf
@@ -1,14 +1,20 @@
+# On utilise les variables ici avec var.nom_de_la_variable
 resource "azurerm_resource_group" "rg_app" {
-  name     = "rg-ma-super-application"
-  location = "westeurope"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-
 resource "azurerm_virtual_network" "vnet_app" {
-  name = "vnet-ma-super-application"
-  # Remarque la magie d'IaC : on ne tape pas le nom à la main, 
-  # on fait référence au groupe de ressources au-dessus !
+  name                = "vnet-ma-super-application"
   location            = azurerm_resource_group.rg_app.location
   resource_group_name = azurerm_resource_group.rg_app.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = var.vnet_address_space
+}
+
+# --- NOUVELLE RESSOURCE : LE SOUS-RÉSEAU ---
+resource "azurerm_subnet" "subnet_frontend" {
+  name                 = "snet-frontend"
+  resource_group_name  = azurerm_resource_group.rg_app.name
+  virtual_network_name = azurerm_virtual_network.vnet_app.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
